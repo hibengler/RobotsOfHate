ff53ff572dae84d67c4575a4d77160f786446786
Hib Engler <h@killercool.net>
2021-07-31 09:15:59 -0400

    better, but not all the way. Going to try qdist on all

---
+++ core/onecolor_specs.c
@@ -0,0 +1,15 @@
+#include "onecolor_specs.h"
+
+void init_onecolor_specs (omecolor_specs *onec) {
+onec->do_effect=0;
+onec->one_color_program=0;
+onec->gvPositionHandle=0; 
+onec->mPositionHandle=0;  
+onec->mMVPMatrixHandle=0; 
+loadIdentity(&(onec->modelMatrix));
+loadIdentity(&(onec->effectMatrix));
+loadIdentity(&(onec->viewMatrix));  
+loadIdentity(&(onec->projectionMatrix));
+loadIdentity(&(onec->origin_matrix));   
+}
+
